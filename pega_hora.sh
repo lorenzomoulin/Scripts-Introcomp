365990e9ea13f656e7da045240658354140b19f7
lorenzomoulin <lorenzomoulin@gmail.com>
2017-09-29 17:30:41 -0300

    Add files via upload

--- pega_hora.sh
+++ pega_hora.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
+hora_prova='090000'
+numero_prova='1'
+hora_final='190000'
+numero_de_provas='10'
 
 while [ true ] ;
 do 
 	date | cut -d " " -f 4 >hora
 	hora=$(cut -d ':' -f 1 hora)$(cut -d ':' -f 2 hora)$(cut -d ':' -f 3 hora)
-	hora_prova='194900'
-	echo $hora e $hora_prova
+	
+	
 	if [ "$hora" -ge "$hora_prova" ] ; then
-		echo Começou > log
-		break
+		
+		if [ "$numero_prova" -gt 10 ]; then
+			echo "---- daqui a 1 hora o site fecha"
+			break
+			
+		fi
+		echo "Começou a prova numero $numero_prova"> log_$numero_prova
+		
+		hora_prova=`expr $hora_prova + 010000`
+		numero_prova=`expr $numero_prova + 1`
+		
 	fi
+	
 done
+
+if [ "$hora" -ge "$hora_final" ]; then
+			echo "Acabou!!!!!"			
+			break
+fi
